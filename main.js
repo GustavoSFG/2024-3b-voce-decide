bb1d49563984f9d0bb88593c7a3862ef0a1ebe59
Gustavo da Silva Franchin - GUUUHYT <106678386+GustavoSFG@users.noreply.github.com>
2024-08-22 10:38:23 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,64 +9,64 @@ const perguntas = [
         enunciado: "Miguel é um menino que vinha de uma familia negra, só por essa parte Miguel saberia doque enfrentaria em sua vida, Miguel completou seus 6 anos e vai começar seu primeiro dia de aula, somente por chegar no portão em uma escola publica ja foi visto por más olhos. Após o fim das aulas Miguel chegou em casa e foi contar para sua mãe sobre seu primeiro dia de escola, *Oque Miguel deve fazer?*",
         alternativas: [
             {
-                texto:"Contar a sua mãe sobre o ocorrido",
+                texto: "Contar a sua mãe sobre o ocorrido",
                 afirmacao: "Depois da fim da aula e sobre os maus olhos quando chegou, Miguel foi correndo para sua mãe e contou tudo, ela ficou muito triste sobre o ocorrido e mandou o Miguel ir para a Aula por mais um dia para ver a reação dos alunos e tomar medidas caso ocorrá novamente."
             },
             {
-                texto:"Mentir para sua mãe sobre o ocorrido",
+                texto: "Mentir para sua mãe sobre o ocorrido",
                 afirmacao: "Depois do fim da aula e sobre os maus olhos, quando Miguel chegou em casa mentiu para sua mãe dizendo que estava tudo bem e a aula foi muito boa e fez amiguinhos novos, sua mãe ficou muito feliz com o ocorrido e deu um abraço nele comemorando oque conseguiu."
-            }   
-        ]
-    },
-    {
-        enunciado: "Enunciado 02",
-        alternativas: [
-            {
-                texto: "alternativa 03",
-                afirmacao: "resultado03"
-            },
-            {
-                texto: "alternativa 04",
-                afirmacao: "resultado 04"
             }
         ]
     },
     {
-        enunciado: "Enunciado 03",
+        enunciado: "No mesmo dia Miguel foi para seu quarto e refletiu sobre o ocorrido na escola e busca como solucionar o que havia com ele.",
         alternativas: [
             {
-                texto: "alternativa 05",
-                afirmacao: "resultado 05"
+                texto: "Miguel tentará fazer amizades novas para ter um grupinho de amigos",
+                afirmacao: "Miguel foi para a escola no dia seguinte e buscou amizade com pessoas iguais a ele e conseguiu."
             },
             {
-                texto: "alternativa 06",
-                afirmacao: "resultado 06"
+                texto: "Miguel ficou muito triste por ter mentido para sua mãe e tentara buscar novas amizades igual ele na escola.",
+                afirmacao: "Miguel voltou para sua mãe e contou sobre a mentira e ela ficou muito triste e apoiou ele sobre buscar novos amigos no colegio e felizmente ele conseguiu"
             }
         ]
     },
     {
-        enunciado: "Enunciado 04",
+        enunciado: "Com seus novos amigos da mesma cor, Miguel então tentara dar uma volta por cima sobre o ocorrido, oque ele precisará fazer?",
         alternativas: [
             {
-                texto: "alternativa 07",
-                afirmacao: "resultado 07"
+                texto: "Fazer uma denuncia para a diretora e pedir para realizarem uma palestra sobre racismo",
+                afirmacao: "Então Miguel foi até a Diretora contou tudo oque aconteceu e pediu a palestra sobre racismo e a diretora aceitou como lição para os alunos que não possuem respeito."
             },
             {
-                texto: "alternativa 08",
-                afirmacao: "resultado 08"
+                texto: "Miguel entra xingando com seu grupo para pessoas que não gosta dele",
+                afirmacao: "Miguel acaba xingando muita gente e vai para a diretoria e leva uma advertencia e um castigo por ultrapassar os limites de xingamento e sua mãe é chamada"
             }
         ]
     },
     {
-        enunciado: "Enunciado 05",
+        enunciado: "Logo depois de todos o acontecimento Miguel é chamado para a sala do diretor e verá oque acontecera com ele.",
         alternativas: [
             {
-                texto: "alternativa 09",
-                afirmacao: "resultado 09"
+                texto: "Miguel vai a sala da diretora",
+                afirmacao: "Miguel é parabenizado pela sua atitude e ganhará vale lanche, passeio para sua turma, e notas extras por ser um exemplo na escola"
             },
             {
-                texto: "alternativa 10",
-                afirmacao: "resultado 10"
+                texto: "Miguel não vai a sala da diretora",
+                afirmacao: "Miguel acaba sendo punido do colegio e perderá sua vaga por desobedecer a diretora e seus superiores"
+            }
+        ]
+    },
+    {
+        enunciado: "Migue seu futuro..",
+        alternativas: [
+            {
+                texto: "Apos crescer Miguel ira se tornar um presidente e lutara contra o racismo no Brasil",
+                afirmacao: "Miguel seguindo sua vida como um exemplo de notas escolares e repeito em seu bairro aos 38 anos se torna um Presidente da Republica do Brasil e lutará contra o racismo"
+            },
+            {
+                texto: "Miguel desiste de tudo e ficará em depressão até alguem ajudar ele.",
+                afirmacao: "Após seu fracasso e detonando sua imagem publica Miguel busca tentar ajuda mas acaba caindo em depressao até buscar Jesus Cristo que ira ajudalo a ter sua cura buscar ser uma nova pessoa na sociedade."
             }
         ]
     },
@@ -76,8 +76,8 @@ let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
-function mostrarPerguntas(){
-    if(atual >= perguntas.length){
+function mostrarPerguntas() {
+    if (atual >= perguntas.length) {
         mostraResultado();
         return
     }
@@ -87,8 +87,8 @@ function mostrarPerguntas(){
     mostrarAlternativas();
 }
 
-function mostrarAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
+function mostrarAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa))
@@ -96,14 +96,14 @@ function mostrarAlternativas(){
     }
 }
 
-function respostaSelecionada(opcaoSelecionada){
+function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = opcaoSelecionada.afirmacao;
     historiaFinal += afirmacoes + " ";
     atual++;
     mostrarPerguntas();
 }
 
-function mostraResultado(){
+function mostraResultado() {
     caixaPerguntas.textContent = " Miguel iniciou sua jornada nesse mundo sem respeito e igualdade, vindo de uma familia negra e pobre mas com condições necessarias, sabia que poderia viver mas não sabia sobre os julgamentos dos outros, Miguel possuindo seus 6 anos começará seu primeiro dia de aula em uma escola publica."
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
